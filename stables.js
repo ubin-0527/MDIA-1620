94f28103830b06811e7d18f19d9df62849ea8e42
ubin-0527 <ujung@my.bcit.ca>
2024-09-19 15:44:39 -0700

    horse

---
+++ stables.js
@@ -0,0 +1,12 @@
+console.log("horse are great")
+
+let horseName = "Coolguy"; // this is a comment or a note
+horseName = "Shazam"
+let horseAge = 5;
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable = true;
+console.log(isHorseInStable);
+isHorseInStable = false;
+horseName = "Coolhorse";
+console.log(horseName);
