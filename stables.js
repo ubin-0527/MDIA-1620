43e35fa1e284a3700bd13832a8e9aac3f61cc86b
ubin-0527 <ujung@my.bcit.ca>
2024-09-26 14:37:23 -0700

    add operators to my stable

--- stables.js
+++ stables.js
@@ -1,12 +1,58 @@
 console.log("horse are great")
 
 let horseName = "Coolguy"; // this is a comment or a note
+console.log(horseName);
 horseName = "Shazam"
 let horseAge = 5;
+console.log(horseName);
+console.log(horseName);
 console.log(horseName);
 console.log(horseAge);
 let isHorseInStable = true;
 console.log(isHorseInStable);
 isHorseInStable = false;
 horseName = "Coolhorse";
-console.log(horseName);
+
+console.log(typeof "42");
+
+console.log(typeof horseName);
+
+console.log(typeof "true");
+
+console.log(typeof 234234);
+console.log(typeof "23");
+console.log(typeof false);
+
+console.log(typeof horseAge);
+
+const STABLE_MONTHLY_FEE = 100;
+console.log(STABLE_MONTHLY_FEE);
+
+console.log(STABLE_MONTHLY_FEE - horseAge);
+
+console.log(STABLE_MONTHLY_FEE * 3);
+
+let horseAgeIn10Years = horseAge + 10;
+let horseAgeIn20Years = horseAgeIn10Years + 10;
+
+console.log(horseAgeIn20Years);
+let horseIntroduction = " is the name of my horse"
+// let rentIntroduction = " it costs " + STABLE_MONTHLY_FEE + " to board " + horseName;
+// let stableIntroduction = "Welcome to " + horseName + "'s stable";
+let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`;
+let stableIntroduction = `Welcome to "${horseName}'s" stable`;
+console.log(horseName + horseIntroduction);
+console.log(rentIntroduction);
+console.log(stableIntroduction);
+console.log(rentIntroduction);
+
+
+
+
+
+
+
+
+
+
+
