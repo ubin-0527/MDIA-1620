7044ea47382b22da1e6774658052c6d0b583db18
ubin-0527 <ujung@my.bcit.ca>
2024-10-10 15:29:04 -0700

    New Horses, Arrays

--- problem-file.js
+++ problem-file.js
@@ -5,3 +5,8 @@ The quick brown fox jumps over the lazy dog
 The quick brown fox jumps over the lazy spaghetti
 
 The quick brown fox jumps over the lazy lasagna
+
+The quick brown fox doesn't jump over the lazy dog. It just sits there and does nothing.
+
+Let's also add our new terminal commands we learned!
+
